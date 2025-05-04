af9a2472511b9ce9eb72ae7284e519eab51ac8cd
Nana Addo <Andybampoe.ad@gmail.com>
2025-05-04 14:48:12 +0200

    feat(ebtv-ecommerce): add welcome bar component

---
+++ src/data/socialsData.js
@@ -0,0 +1,36 @@
+// /data/socialsData.js
+import { Twitter, Facebook, Instagram, Youtube, Mail ,LucideIcon } from "lucide-react"
+
+
+export const socials = [
+    {
+        name: "Twitter",
+        href: "https://x.com/everythingbeadedgh",
+        icon: Twitter,
+        ariaLabel: "Follow me on Twitter for updates and insights.",
+    },
+    {
+        name: "Facebook",
+        href: "https://facebook.com/everythingbeadedgh",
+        icon: Facebook,
+        ariaLabel: "Follow me on Facebook.",
+    },
+    {
+        name: "Instagram",
+        href: "https://instagram.com/everythingbeadedgh",
+        icon: Instagram,
+        ariaLabel: "Follow me on Instagram.",
+    },
+    {
+        name: "YouTube",
+        href: "https://youtube.com/@everythingbeadedgh",
+        icon: Youtube,
+        ariaLabel: "Subscribe to my YouTube channel.",
+    },
+    {
+        name: "Email",
+        href: "mailto:everythingbeadedgh@gmail.com",
+        icon: Mail,
+        ariaLabel: "Send me an email to get in touch directly.",
+    },
+]
